91aee0a85d45b36f92b0986d20127d15f30c2a96
Alexander Grudski <alexander.grudski@intel.com>
2025-07-09 17:59:59 +0300

    v9

--- src/app.py
+++ src/app.py
@@ -12,7 +12,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
         'hostaname': socket.gethostname()
-        'message': 'Hello from here v8'
+        'message': 'Hello from here v9'
     })
     
 @app.route('/api/v1/healthz')
