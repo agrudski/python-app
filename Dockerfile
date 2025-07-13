ffd47210b89c22626fc129ffdd5b9b68aa402399
Alexander Grudski <alexander.grudski@intel.com>
2025-07-13 22:25:47 +0300

    Adds backstage catalog

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,4 @@ RUN pip install -r /tmp/requirements.txt
 COPY ./src /src
 
 CMD python /src/app.py
+
--- src/app.py
+++ src/app.py
@@ -6,24 +6,23 @@ import socket
 app = Flask(__name__)
 
 
-@app.route('/api/v1/details')
+@app.route('/api/v1/info')
 
-def details():
+def info():
     return jsonify({
-        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
-        'hostaname': socket.gethostname()
-        'message': 'Hello from here v9'
+    	'time': datetime.datetime.now().strftime("%I:%M:%S%p  on %B %d, %Y"),
+    	'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! <3',
+        'deployed_on': 'kubernetes'
     })
-    
+
 @app.route('/api/v1/healthz')
 
-def healthz():
+def health():
+	# Do an actual check here
     return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
 
     app.run(host="0.0.0.0")
 
-
-# '/api/v1/details'
-# '/api/v1/healthz'
